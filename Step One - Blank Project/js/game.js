e1c7c768e2e65302e3103a310937bea3c32bd2dd
defacid <defacid@gmail.com>
2016-12-03 05:49:25 -0500

    First Step of the Project
    Starting off with a blank project

---
+++ Step One - Blank Project/js/game.js
@@ -0,0 +1,28 @@
+// First, we set the properties of the game: the width and height of the game area
+var properties = {
+    width: 640,
+    height: 480
+};
+
+// Initialize the game as an instance of the Phaser.Game object established in the framework
+// Argument 0: Width
+// Argument 1: Height
+// Argument 2: Renderer - It can be specified to WebGL or Canvas but we'll let the interpreter decide
+// Argument 3: DOM element id in our index.html where the game will be displayed
+// Argument 4: Game states
+var game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
+
+// All assets will be loaded in here
+function preload() {
+
+},
+
+// Initialization of all the sprites, sounds, levels, text, etc.
+function create() {
+
+},
+
+// This is the main game loop. This function is run every frame. (The initial FPS is 60)
+function update() {
+
+};
